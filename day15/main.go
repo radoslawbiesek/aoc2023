aafbcb67c2a97ee47d50db89918feefd2ddf9c43
Radosław Biesek <radoslaw.biesek@gmail.com>
2023-12-15 10:48:42 +0100

    day15 part2

--- day15/main.go
+++ day15/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"slices"
+	"strings"
 
 	"github.com/radoslawbiesek/aoc2023/utils"
 )
@@ -24,16 +26,65 @@ func part1(path string) (total int) {
 	return
 }
 
+type lens struct {
+	label  string
+	length int
+}
+
 func part2(path string) (total int) {
+	boxes := [256][]lens{}
+	for _, line := range utils.GetLines(path, ",") {
+		var operation, label string
+		var length int
+
+		if strings.Contains(line, "-") {
+			operation = "-"
+			label = strings.Replace(line, operation, "", 1)
+		} else {
+			operation = "="
+			splitted := strings.Split(line, operation)
+			label = splitted[0]
+			length = utils.ParseInt(splitted[1])
+		}
+
+		boxIndex := hash(label)
+		box := boxes[boxIndex]
+		index := slices.IndexFunc(box, func(lens lens) bool {
+			return lens.label == label
+		})
+
+		switch operation {
+		case "-":
+			if index != -1 {
+				box = append(box[:index], box[index+1:]...)
+			}
+		case "=":
+			lens := lens{label: label, length: length}
+			if index == -1 {
+				box = append(box, lens)
+			} else {
+				box[index] = lens
+			}
+		}
+
+		boxes[boxIndex] = box
+	}
+
+	for boxIndex, box := range boxes {
+		for lensIndex, lens := range box {
+			total += (boxIndex + 1) * (lensIndex + 1) * lens.length
+		}
+	}
+
 	return
 }
 
 func main() {
 	fmt.Println("Test input: ")
 	fmt.Printf("Part 1: %d\n", part1("./test-input.txt"))
-	// fmt.Printf("Part 2: %d\n", part2("./test-input.txt"))
+	fmt.Printf("Part 2: %d\n", part2("./test-input.txt"))
 	fmt.Println("")
 	fmt.Println("Input: ")
 	fmt.Printf("Part 1: %d\n", part1("./input.txt"))
-	// fmt.Printf("Part 2: %d\n", part2("./input.txt"))
+	fmt.Printf("Part 2: %d\n", part2("./input.txt"))
 }
